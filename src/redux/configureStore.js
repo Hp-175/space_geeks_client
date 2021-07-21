5e6816461b4b18aeaf9c5bc167e9cbb3d87010ba
Hrushi Patel <patelhrushi16@gmail.com>
2021-07-21 15:50:54 +0530

    Redux created

---
+++ src/redux/configureStore.js
@@ -0,0 +1,27 @@
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import { Achievements } from './Achievements';
+import {Facts} from './Facts';
+import {Theories} from './Theories';
+import {FavouriteAchievements} from './FavouriteAchievements';
+import {FavouriteFacts} from './FavouriteFacts';
+import {FavouriteTheories} from './FavouriteTheories';
+import {Auth} from './Auth';
+import thunk from 'redux-thunk';
+import logger from 'redux-logger';
+
+export const ConfigureStore = () => {
+    const store = createStore(
+        combineReducers({
+            achievements: Achievements,
+            facts: Facts,
+            theories:Theories,
+            favouriteAchievements: FavouriteAchievements,
+            favouriteFacts:FavouriteFacts,
+            favouriteTheories:FavouriteTheories,
+            auth: Auth,
+        }),
+        applyMiddleware(thunk, logger)
+    );
+
+    return store;
+}
